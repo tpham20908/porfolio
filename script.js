e5ce330f2bf6ba51cfd8f30fa914066aa75bdbb5
unknown <tpham20908@gmail.com>
2018-01-19 21:37:51 -0500

    setup hmtl, css, js

---
+++ script.js
@@ -0,0 +1,7 @@
+$(function() {
+  var backgroundImg = [
+    "https://images.unsplash.com/photo-1432405972618-c60b0225b8f9?auto=format&fit=crop&w=1550&q=80",
+    "https://images.unsplash.com/photo-1473101167633-bfa08e705e4b?auto=format&fit=crop&w=1550&q=80"
+  ];
+  $("body").css("background-image", "url(" + backgroundImg[0] + ")" );
+});
