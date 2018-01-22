4adf34ddbcee9d0d5fbbef50e230207ec609eb86
unknown <tpham20908@gmail.com>
2018-01-22 18:35:34 -0500

    make function to display ul#nav at max width 768px

--- script.js
+++ script.js
@@ -4,4 +4,21 @@ $(function() {
     "https://images.unsplash.com/photo-1473101167633-bfa08e705e4b?auto=format&fit=crop&w=1550&q=80"
   ];
   $("body").css("background-image", "url(" + backgroundImg[0] + ")" );
+
+  var menuVisible = false;
+  $("#button").click(function() {
+    if (menuVisible) {
+      $("#navbar").addClass("pull-right");
+      $("#navbar").css({"display" : "none"});
+      menuVisible = false;
+      return;
+    }
+    $("#navbar").css({"display" : "block"});
+    menuVisible = true;
+  });
+  $("#navbar").click(function() {
+    $(this).css({"display" : "none"});
+    menuVisible = false;
+  });
 });
+
