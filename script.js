e06148d5d588f650064d77a318d5670b5a7f18b1
tpham20908 <tpham20908@gmail.com>
2019-01-25 23:03:05 -0500

    added phone project

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(function() {
     "https://images.unsplash.com/photo-1432405972618-c60b0225b8f9?auto=format&fit=crop&w=1550&q=80",
     "https://images.unsplash.com/photo-1473101167633-bfa08e705e4b?auto=format&fit=crop&w=1550&q=80"
   ];
-  $("body").css("background-image", "url(" + backgroundImg[0] + ")" );
+  $("body").css("background-image", "url(" + backgroundImg[1] + ")" );
 
   var menuVisible = false;
   $("#button").click(function() {
